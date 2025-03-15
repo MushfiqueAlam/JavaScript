9e8f1092d1fb765a4c196dc386430c7f2319b0b4
MushfiqueAlam <mushfique.037@gmail.com>
2025-03-16 00:06:31 +0530

    javascript concept

---
+++ first.js
@@ -0,0 +1,26 @@
+//  console.log("Mushfique");
+// fullname="MushfiqueAlam";
+// console.log(fullname);
+// let age=21;
+// console.log(age);
+// let price=600.5;
+// console.log(price);
+// x=null;
+// y=undefined;
+// yes=true;
+// no=false;
+// console.log(x);
+// console.log(y)
+// console.log(yes);
+// console.log(no);
+////Arrow funcyion
+// let sum=(a,b) =>{
+//     return a+b;
+// }
+// console.log(sum(4,3));
+// let arr=[30,50,90,5650];
+// let ans=arr.every((el)=>{
+//     return el%10==0;
+// });
+
+console.log("Mushfique");
