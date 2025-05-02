56c7d1e8d4e3ff5f66782402492247985b917e5d
MushfiqueAlam <mushfique.037@gmail.com>
2025-05-03 01:54:25 +0530

    changes

--- javaScriptConcept/dom/script.js
+++ javaScriptConcept/dom/script.js
@@ -1,2 +1,51 @@
-console.log("Hello World");
-console.log("I am Mushfique");
+// console.log("Hello World");
+// console.log("I am Mushfique");
+
+// console.log(window.document)  //Element and tags in the document
+// console.dir(document);//object and its methods and properties
+
+
+// DOM Manipulation
+// Selection by id
+let button=document.getElementById("myBtn");
+// console.log(button); //get the button element by id
+
+// Selection by class name
+let class1=document.getElementsByClassName("myClass");
+// console.log(class1); //get the button element by class name
+
+
+
+// Selection by tag name
+let tag1=document.getElementsByTagName("p");
+// console.log(tag1); //get the button element by tag name
+
+
+
+
+// Selection by query selector
+let myBtn=document.querySelector("#myBtn"); //get the button element by id
+// console.log(myBtn); //get the button element by id
+
+let myClass=document.querySelector(".myClass"); //get the button element by class name
+// console.log(myClass); //get the button element by class name
+
+
+
+
+// Selection by query selector all
+let myClass1=document.querySelectorAll(".myClass"); //get the button element by class name
+// console.log(myClass1); //get the button element by class name
+
+
+
+
+// Properties
+//  1. tagName: it returns the tag name of the element
+//  2. id: it returns the id of the element
+//  3. className: it returns the class name of the element
+//  4. innerHTML: it returns the inner HTML of the element
+//  5. innerText: it returns the inner text of the element  
+//  6. outerHTML: it returns the outer HTML of the element
+
+
