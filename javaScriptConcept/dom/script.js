c4b7aef31da38c56c9905176f809ad3b10959b58
MushfiqueAlam <mushfique.037@gmail.com>
2025-05-03 17:50:31 +0530

    dom

--- javaScriptConcept/dom/script.js
+++ javaScriptConcept/dom/script.js
@@ -47,5 +47,60 @@ let myClass1=document.querySelectorAll(".myClass"); //get the button element by 
 //  4. innerHTML: it returns the inner HTML of the element
 //  5. innerText: it returns the inner text of the element  
 //  6. outerHTML: it returns the outer HTML of the element
+//  7. textContent: it returns the text content of the element
 
+
+
+// let div=document.querySelector("div");
+// console.log(div); //get the tag name of the element
+// console.dir(div); //get the object of the element
+
+let heading=document.querySelector("h2");
+// console.dir(heading); //get the tag name of the element
+
+
+
+let h1=document.querySelector("h1");
+// console.log(h1.innerText)
+
+// let divs=document.querySelectorAll(".box");
+// divs[0].innerHTML="Random text for div 1";
+// divs[1].innerHTML="Random text for div 2";
+// divs[2].innerHTML="Random text for div 3";
+
+// for(let i=0;i<divs.length;i++){
+//     divs[i].innerText="Random text for div "+(i+1);
+    
+// }
+
+
+
+
+
+// DOM Manipulation
+// Attributes
+// 1. getAttribute: it returns the value of the attribute
+// 2. setAttribute: it sets the value of the attribute
+
+// let div=document.querySelector("div");
+// let id=div.getAttribute("id"); //get the id of the element
+// let name=div.getAttribute("name"); //get the name of the element
+// console.log(div); //get the tag name of the element
+// console.log(id); //get the id of the element
+// console.log(name); //get the name of the element
+
+// let para=document.querySelector("p");
+// console.log(para)
+// console.log(para.getAttribute("class")); //get the class of the element
+
+
+// let para1=document.querySelector("p");
+// console.log(para1.setAttribute("class","myClass1")); //set the class of the element
+// console.log(para1.getAttribute("class")); //get the class of the element
+
+// style to access all the inline styles of the element
+// let div=document.querySelector("div");
+// div.style.color="red"; //set the color of the element
+// div.style.backgroundColor="blue"; //set the background color of the element
+// div.style.fontSize="50px";
 
